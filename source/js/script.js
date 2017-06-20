263578ad489808410de420829262fd1f1314ce4c
dskuzm <dskuzm@gmail.com>
2017-06-20 17:11:49 +0300

    добавил проект

---
+++ source/js/script.js
@@ -0,0 +1,266 @@
+//burger-menu
+$(function () {
+    $('.burger-icon').click(function () {
+        $('body').toggleClass('no-scroll');
+    }),
+    $('.nav a').click(function () {
+        $('body').removeClass('no-scroll');
+    });
+});
+//end-burger-menu
+//reviews--slider
+$('.reviews-slider').slick({
+    slidesToShow: 3
+    , slidesToScroll: 3
+    , arrows: false
+    , dots: true
+    , responsive: [
+        {
+            breakpoint: 992
+            , settings: {
+                slidesToShow: 2
+                , slidesToScroll: 2
+            , }
+    }, {
+            breakpoint: 768
+            , settings: {
+                slidesToShow: 1
+                , slidesToScroll: 1
+            }
+    }
+  ]
+});
+//end-reviews--slider
+//brands-slider
+$('.brands-slider').slick({
+    slidesToShow: 1
+    , slidesToScroll: 1
+    , arrows: false
+    , dots: true
+});
+//end-brands-slider
+
+//map
+var map;
+var egglabs = new google.maps.LatLng(40.723906, -73.942898);
+var mapCoordinates = new google.maps.LatLng(40.723906, -73.942898);
+
+function initialize() {
+  var mapOptions = {
+	backgroundColor: "#ffffff",
+    zoom: 14,
+	disableDefaultUI: true,
+      scrollwheel: false ,
+    center: mapCoordinates,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "stylers": [
+      {
+        "weight": 1.5
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+          "color":"#2b2b2b"
+
+      }
+    ]
+  }
+]
+  };
+map = new google.maps.Map(document.getElementById('map'),mapOptions);
+
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+//end-map
+//scroll
+$(document).ready(function(){
+    $('.goTo').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
+});
+//end-scroll
